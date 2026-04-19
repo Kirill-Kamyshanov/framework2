a8c416e1d797445b5af08fdc1b37adf446b88ba5
kirill_kamyshanov <nestor10000@mail.ru>
2026-04-19 09:45:44 +0300

    fixed notifications

--- tests/test_get_user_validation.py
+++ tests/test_get_user_validation.py
@@ -1,20 +1,11 @@
 from services.reqres_in.users.get_user import GetUser
-from pydantic import BaseModel, Field, ValidationError
-
+from models.user import User
 
 def test_user_validation(env_config):
-    class User(BaseModel):
-        id: int = Field(gt=0)
-        email: str
-        first_name: str
-        last_name: str
-        avatar: str
-
     user_id = 1
     full_response_json = GetUser(env_config).get_user(user_id).json()
 
-    try:
-        User(**full_response_json["data"])
-        print(f'Validation user {user_id} succeeded.')
-    except ValidationError as e:
-        print(f'Validation user {user_id} failed.')
+    validate = User(**full_response_json["data"])
+    assert validate.id == user_id, f'invalid user_id: {user_id}'
+    print(f'Validation user {user_id} succeeded.')
+
