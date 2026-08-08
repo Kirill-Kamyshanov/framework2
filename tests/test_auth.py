e7f418ec364172a74ebea753c786d46ea28b6b89
Kirill-Kamyshanov <nestor10000@mail.ru>
2026-08-08 14:15:54 +0300

    updated CI/CD config

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -37,6 +37,7 @@ class TestAuth:
 
         with allure.step("Валидация ответа"):
             assert_register_failed(response, validated, test_data["auth"]["expected_error"])
+            assert False
 
     @pytest.mark.smoke
     @pytest.mark.regression
